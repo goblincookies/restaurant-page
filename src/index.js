9b5ac490f8cc6116a072aa1a197c75f4e886fff2
goblincookies <goblincookiebox@gmail.com>
2025-02-24 12:53:20 -0500

    working webpack setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css"
+import { testLink } from "./testLink.js"
+
+
+let link = new testLink();
+
+link.shout();
+
+console.log("hello");
---
+++ src/testLink.js
@@ -0,0 +1,20 @@
+// const testLink = ( function () {
+
+//     const shout = function () {
+//         console.log("linked :)")
+//     };
+
+//     return( shout );
+// })();
+
+class testLink {
+
+    shout = function () {
+        console.log("linked :)")
+    };
+
+};
+
+export { testLink }
+
+// console.log("testlink.js")
