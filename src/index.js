c4c77dde4ecda249c99f39acfbda8dabb19aa34a
goblincookies <goblincookiebox@gmail.com>
2025-02-24 14:42:32 -0500

    has template HTML; initial CSS

--- src/index.js
+++ src/index.js
@@ -7,3 +7,10 @@ let link = new testLink();
 link.shout();
 
 console.log("hello");
+
+document.getElementById("story").addEventListener("click", ourStory);
+
+
+function ourStory() {
+    console.log("check it out!")
+}
